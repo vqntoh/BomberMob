61bec7389edde1bd2f174cd2848f7b075d44cb82
vqntoh <cavalleris.daniel@gmail.com>
2024-06-19 19:31:19 +0200

    Improved task readability by using streams.

--- src/main/java/me/sedano/bombermob/tasks/BomberTask.java
+++ src/main/java/me/sedano/bombermob/tasks/BomberTask.java
@@ -2,19 +2,16 @@ package me.sedano.bombermob.tasks;
 
 import me.sedano.bombermob.BomberMob;
 import org.bukkit.Bukkit;
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class BomberTask extends BukkitRunnable {
     @Override
     public void run() {
-        Bukkit.getWorlds().forEach(world -> {
-            for (LivingEntity entity : world.getLivingEntities()) {
-                if (!entity.getPersistentDataContainer().has(BomberMob.Keys.BOMBER_MOB_KEY, PersistentDataType.STRING))
-                    continue;
-                entity.getWorld().createExplosion(entity, entity.getLocation(), 3.0F, false);
-            }
-        });
+        Bukkit.getWorlds()
+                .stream()
+                .flatMap(world -> world.getLivingEntities().stream())
+                .filter(livingEntity -> livingEntity.getPersistentDataContainer().has(BomberMob.Keys.BOMBER_MOB_KEY, PersistentDataType.STRING))
+                .forEach(livingEntity -> livingEntity.getWorld().createExplosion(livingEntity, livingEntity.getLocation(), 3.0F, false));
     }
 }
